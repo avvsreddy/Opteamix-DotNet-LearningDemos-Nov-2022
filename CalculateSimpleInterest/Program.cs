5ad40041e75e4ec9e8b58e22b6d1dc6230a506ba
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-11-03 17:42:04 +0530

    C# Ref

--- CalculateSimpleInterest/Program.cs
+++ CalculateSimpleInterest/Program.cs
@@ -7,15 +7,15 @@
             Console.WriteLine("Simple Interest Calculator");
             Console.WriteLine("============================");
             Console.Write("Enter Principle Amount: ");
-            int principleAmount = int.Parse(Console.ReadLine());
+            double principleAmount = double.Parse(Console.ReadLine());
             Console.Write("Enter Rate of Interest: ");
-            int interestRate = int.Parse(Console.ReadLine());
+            double interestRate = double.Parse(Console.ReadLine());
             Console.WriteLine("Enter Time/Duration: ");
-            int time = int.Parse(Console.ReadLine());
+            double time = double.Parse(Console.ReadLine());
 
             // calculate the simple interest
             // 
-            float simpleInterest = principleAmount * time * interestRate / 100;
+            double simpleInterest = principleAmount * time * interestRate / 100;
             Console.WriteLine($"The Simple Interest is {simpleInterest}");
         }
     }
